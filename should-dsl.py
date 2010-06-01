c7ab7ebb828865502f528254f779a3b0669de09e
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-06-01 10:20:12 -0300

    Add should-dls matchers

--- should-dsl.py
+++ should-dsl.py
@@ -16,6 +16,26 @@
     - throw
     - thrown_by
 
+# should be (b)
+${1:actual} |should| be(${2:expected})
+$0
+
+# should not be (nb)
+${1:actual} |should_not| be(${2:expected})
+$0
+
+# Should have (h)
+${1:collection} |should| have(${2:items})
+$0
+
+# Should not have (nh)
+${1:collection} |should_not| have(${2:items})
+$0
+
+# Should not be into (i)
+${1:items} |should_not| be_into(${2:collection})
+$0
+
 # Should be equal to (et)
 ${1:actual} |should| be_equal_to({2:expect})
 $0
