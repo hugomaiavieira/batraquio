d29639177964325087dae0252158d1167a060d9f
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-08-25 16:45:39 -0300

    'Scape HTML tags' snippet refactored.

--- src/scape_html_tags.py
+++ src/scape_html_tags.py
@@ -1,9 +1,7 @@
 #$<
 def scape_html_tags(code):
-    code = code.replace('<','&lt;')
-    code = code.replace('>','&gt;')
-#    code = code.replace('\>','&gt;') # Comentar a linha de cima
-    return code
+    return code.replace('<','&lt;').replace('>','&gt;')
+#    return code.replace('<','&lt;').replace('\>','&gt;') # Comentar a linha de cima
 
 #return scape_html_tags($GEDIT_SELECTED_TEXT)
 #>
